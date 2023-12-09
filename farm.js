b52a4b025c4b9e5fad516904c09a38b2818c7db0
ferky01 <korgeov@gmail.com>
2023-12-09 17:48:20 +0300

    Update farm.js

--- farm.js
+++ farm.js
@@ -1,34 +1,65 @@
-// Массив с координатами для фарминга
+// Регистрируем корневую папку меню
+Menu.RegisterRootFolder("Farm Script");
+
+// Папка в меню
+const settings = Menu.AddFolder("Farm Script", "Settings"); 
+
+// Массив точек для фарма
 const farmSpots = [
-  new Vector(-1500, 1500, 0), 
-  new Vector(-2000, -2000, 0)
+  new Vector(-1500, 1500),
+  new Vector(-2000, -2000) 
 ];
 
-// Функция для отправки юнита фармить в случайную точку
-function sendUnitToFarm(unit) {
+// Добавляем тогл в настройки   
+const enabled = settings.AddToggle("Enabled", false);
 
-  // Выбираем случайную фарм точку
-  const spot = farmSpots[Math.floor(Math.random() * farmSpots.length)]; 
-  
-  // Отправляем приказ юниту идти в эту точку
-  unit.Order({
+// Инициализация скрипта
+function OnScriptLoad() {
+
+  // Вешаем обработчик на переключение тоглла
+  enabled.OnChange(() => {   
+    // перезаписываем переменную 
+    enabled.value = !enabled.value;
+  });
+
+}
+
+
+// Обработка нажатия клавиши F
+GameEvents.Subscribe("keyevent", (key) => {
+
+  // Если скрипт выключен - выходим
+  if (!enabled.value) 
+    return; 
+    
+  // Если нажата F  
+  if (key === EKeyCode.F) {
+
+    // Отправляем всех выделенных юнитов
+    Players.GetLocal().GetSelectedUnits().ForEach(unit => {
+     SendUnitToFarm(unit) 
+    });
+
+  }
+   
+});
+
+// Отправка юнита фармить 
+function SendUnitToFarm(unit) {
+
+  // Выбираем случайную точку
+  const spot = farmSpots[Math.RandomInt(0, farmSpots.length)];
+
+  // Приказ юниту двигаться в эту точку  
+  unit.Order({  
     OrderType: dotaunitorder_t.DOTA_UNIT_ORDER_MOVE_TO_POSITION,
     Position: spot
   });
+
 }
 
-// Обработчик события нажатия клавиши "F"
-GameEvents.Subscribe("keyevent", (key) => {
 
-  // Проверяем, если "F"
-  if (key === EKeyCode.F) {
-
-    // Получаем выделенных юнитов
-    const units = Players.GetLocal().GetSelectedUnits();
-
-    // Отправляем каждого фармить 
-    for (const unit of units) {
-      sendUnitToFarm(unit); 
-    }
-  }
-});
+// Регистрируем скрипт
+RegisterScript({
+  OnScriptLoad    
+}, "Farm Script");
